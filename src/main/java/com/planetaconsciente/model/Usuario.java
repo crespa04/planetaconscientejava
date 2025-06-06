f435d5aae9801d044d2653b2ff79b91e4bcf87ec
Ldforluis <luis0310floresc@gmail.com>
2025-06-05 22:56:04 -0500

    hola

--- src/main/java/com/planetaconsciente/model/Usuario.java
+++ src/main/java/com/planetaconsciente/model/Usuario.java
@@ -10,7 +10,7 @@ public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String usernam;
+    private String username;
     private String password;
     private String email;
     
