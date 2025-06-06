93282fc5f68d435b13fec25eeafc4ac99dc3ed82
Ldforluis <luis0310floresc@gmail.com>
2025-06-05 23:31:38 -0500

    cambios el mejor

--- src/main/java/com/app/planetaconsciente/repository/UsuarioRepository.java
+++ src/main/java/com/app/planetaconsciente/repository/UsuarioRepository.java
@@ -1,7 +1,8 @@
-package com.planetaconsciente.repository;
+package com.app.planetaconsciente.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.planetaconsciente.model.Usuario;
+
+import com.app.planetaconsciente.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Usuario findByUsername(String username);
