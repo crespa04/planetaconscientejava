5eb5929f1c9ff3872bfabcb5957d4cd059ea2e14
santiago <memesanti77@gmail.com>
2025-06-05 22:24:12 -0500

    santiago

---
+++ src/main/java/com/planetaconsciente/PlanetaconscienteApplication.java
@@ -0,0 +1,13 @@
+package com.planetaconsciente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanetaconscienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanetaconscienteApplication.class, args);
+	}
+
+}
