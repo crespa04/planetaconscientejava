bd2f4e969bd55fd7edd56a3eb4c84feef59caedd
Jeffeyeless <jeffedeel@gmail.com>
2025-06-09 22:59:25 -0500

    asad

--- src/main/java/com/app/planetaconsciente/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.planetaconsciente.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.planetaconsciente.model.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    Usuario findByUsername(String username);
-}
